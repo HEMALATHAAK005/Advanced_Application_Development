62a640c24a3d58dd7f53e1c2b3cbbb7c793f4993
HEMALATHAAK005 <110532264+HEMALATHAAK005@users.noreply.github.com>
2024-03-20 16:24:14 +0530

    Add files via upload

---
+++ Day_6/boathouse/src/test/java/com/boat/boathouse/BoathouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boat.boathouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoathouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
